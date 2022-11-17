25f80147741ca755524db91f2295d792dc3a9528
Luan Peres <oluanperes@gmail.com>
2022-11-16 20:50:44 -0400

    Created database

--- stage-08/api/src/server.js
+++ stage-08/api/src/server.js
@@ -1,4 +1,5 @@
 require("express-async-errors");
+const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
 
 const express = require("express");
@@ -8,6 +9,8 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+migrationsRun();
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
