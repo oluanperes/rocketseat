2919fc706d65572ddd496d666c67c9b2349139cc
Luan Peres <oluanperes@gmail.com>
2022-11-16 21:47:13 -0400

    assign usersController.update

--- stage-08/api/src/routes/users.routes.js
+++ stage-08/api/src/routes/users.routes.js
@@ -2,13 +2,9 @@ const { Router } = require("express");
 const UsersController = require("../controllers/UsersController");
 const usersRoutes = Router();
 
-function myMiddleware(request, response, next) {
-  
-  next();
-}
-
 const usersController = new UsersController();
 
-usersRoutes.post("/", myMiddleware, usersController.create);
+usersRoutes.post("/", usersController.create);
+usersRoutes.put("/:id", usersController.update);
 
 module.exports = usersRoutes;
