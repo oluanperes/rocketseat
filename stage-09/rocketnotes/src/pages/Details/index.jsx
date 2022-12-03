4ad653eaa1364ac079b908d70ee2ba4404f18dfe
Luan Peres <oluanperes@gmail.com>
2022-12-03 10:19:25 -0400

    created tag and marcadores components

--- stage-09/rocketnotes/src/pages/Details/index.jsx
+++ stage-09/rocketnotes/src/pages/Details/index.jsx
@@ -1,11 +1,27 @@
-import { Container } from './styles';
+import { Container, Links } from './styles';
 
+import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
+import { Section } from '../../components/Section';
+import { Tag } from '../../components/Tag';
 
 export function Details() {
   return(
     <Container>
-      <h1>Teste</h1>
+      <Header />
+      
+      <Section title="Links úteis">
+        <Links>
+          <li><a href='#'>https://wwww.rocketseat.com.br/</a></li>
+          <li><a href='#'>https://wwww.rocketseat.com.br/</a></li>
+        </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express"/>
+        <Tag title="nodeJS"/>
+
+      </Section>
 
       <Button title="Login" loading />
     </Container>
