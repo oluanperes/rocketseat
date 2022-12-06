aec750339c66d2ce828d13780e37ea01b2bce568
Luan Peres <oluanperes@gmail.com>
2022-12-06 19:51:30 -0400

    created Input component

--- stage-09/rocketnotes/src/pages/Home/index.jsx
+++ stage-09/rocketnotes/src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import { FiPlus } from 'react-icons/fi';
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 import { Header } from '../../components/Header';
+import { Input } from '../../components/Input';
 import { ButtonText } from '../../components/ButtonText';
 
 
@@ -20,7 +21,7 @@ export function Home() {
       </Menu>
 
       <Search>
-
+        <Input placeholder="Pesquisar pelo tiíulo"/>
       </Search>
 
       <Content>
