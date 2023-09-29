fa3dbd37d7fe2ba408aefdba0afd9b5985bb0314
Luan Peres <oluanperes@gmail.com>
2023-09-28 22:50:36 -0400

    created the ignite project

---
+++ ignite/shop/src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import logoImg from "../assets/logo.svg";
+import { AppProps } from "next/app";
+import { globalStyles } from "@/styles/global";
+import { Container, Header } from "@/styles/pages/app";
+
+globalStyles();
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
+}
